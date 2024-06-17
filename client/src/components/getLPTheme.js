994f7322dc0c8e4cdc1dec7e08cf984a64e2e21a
kylekmcleod <kylekmcleod1@gmail.com>
2024-06-17 10:49:36 -0700

    Fixed text field animation + shadow

--- client/src/components/getLPTheme.js
+++ client/src/components/getLPTheme.js
@@ -559,6 +559,7 @@ export default function getLPTheme(mode) {
               },
             },
             '& .MuiOutlinedInput-root': {
+            
               boxSizing: 'border-box',
               minWidth: 280,
               minHeight: 40,
@@ -573,27 +574,32 @@ export default function getLPTheme(mode) {
                 background: `${alpha('#FFF', 0.3)}`,
               },
               '&:hover': {
-                borderColor: brand[300],
+                borderColor: brand[300], 
               },
               '&.Mui-focused': {
-                borderColor: brand[400],
-                outline: '4px solid',
-                outlineColor: brand[200],
+                borderColor: 'transparent',
+                
+                '& fieldset': {
+                  borderColor: brand[400],
+                },
               },
+            },
+            '& .MuiInputLabel-outlined.Mui-focused': {
+              transform: 'translate(14px, -6px) scale(0.75)', // Adjust based on your design
+            },
+            '& .MuiInputLabel-outlined.MuiInputLabel-shrink': {
+              transform: 'translate(14px, -6px) scale(0.75)', // Adjust based on your design
             },
             ...(theme.palette.mode === 'dark' && {
               '& .MuiOutlinedInput-root': {
                 boxSizing: 'border-box',
+                borderColor: gray[600],
                 minWidth: 280,
                 minHeight: 40,
-                height: '100%',
-                borderRadius: '10px',
                 border: '1px solid',
-                borderColor: gray[600],
                 transition: 'border-color 120ms ease-in',
                 '& fieldset': {
-                  border: 'none',
-                  boxShadow: ' 0px 2px 4px rgba(0, 0, 0, 0.4)',
+                  boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.4)',
                   background: `${alpha(gray[800], 0.4)}`,
                 },
                 '&:hover': {
@@ -601,8 +607,9 @@ export default function getLPTheme(mode) {
                 },
                 '&.Mui-focused': {
                   borderColor: brand[400],
-                  outline: '4px solid',
-                  outlineColor: alpha(brand[500], 0.5),
+                  '& fieldset': {
+                    borderColor: theme.palette.primary.main,
+                  },
                 },
               },
             }),
