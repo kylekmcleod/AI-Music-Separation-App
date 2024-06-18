1f90c018810d08afe34c710dfa333421672b5447
Kyle McLeod <kylekmcleod1@gmail.com>
2024-06-18 11:36:42 -0700

    Add required fields to user model

--- server/models/user.js
+++ server/models/user.js
@@ -3,16 +3,20 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName:{
         type: String,
+        required: true
     },
     lastName:{
         type: String,
+        required: true
     },
     email:{
         type: String,
         unique: true,
+        required: true
     },
     password:{
         type: String,
+        required: true
     },
     credits:{
         type: Number,
